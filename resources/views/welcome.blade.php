72b254732364ff3928227cd0962c2dc84f3d0dd8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-14 19:24:39 +0530

    UPdate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,14 @@
 
     @include('header')
 
+    <style>
+        .home-content{
+            background-image: url(https://wallpaperaccess.com/full/2140.jpg);
+            width: 100vh
+            
+        }
+    </style>
+
     <body class="antialiased">
         {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
@@ -30,6 +38,11 @@
         <div class="container">
             {{-- All Videoss
             <a href="{{ route('login') }}">login here</a> --}}
+
+            <div class="home-content">
+
+            </div>
+
         </div>
 
     </body>
