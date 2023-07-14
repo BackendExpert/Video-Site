0d3a84cab80ec0bc4cd085ada5315ef62f5ea0c2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-14 19:31:34 +0530

    Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,12 @@
+
+
 <footer class="footer">
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
-                
+                <div class="footer-head">
+                    About
+                </div>
             </div>
             <div class="col-lg-4"></div>
             <div class="col-lg-4"></div>
