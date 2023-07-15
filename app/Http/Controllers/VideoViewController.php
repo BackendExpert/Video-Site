3d7e16bbd4473631ebe23ee6799ac03a8d3be79e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-15 17:55:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VideoViewController.php
+++ app/Http/Controllers/VideoViewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\VideoM;
 
 class VideoViewController extends Controller
 {
@@ -11,7 +12,8 @@ class VideoViewController extends Controller
      */
     public function index()
     {
-        
+        $video = VideoM::all();        
+        return view('videos.index', compact('video'));
     }
 
     /**
