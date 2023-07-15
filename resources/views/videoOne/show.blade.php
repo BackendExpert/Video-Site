823378156a8a1f29135c722ca3ebf94719e95e7a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-15 18:10:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/videoOne/show.blade.php
+++ resources/views/videoOne/show.blade.php
@@ -4,13 +4,16 @@
     .one-video{
         padding: 40px 0;
     }
+    .view-video{
+        width: 100%;
+    }
 </style>
 
 <div class="container">
     <div class="one-video">
         <a href="{{ url('') }}"><button class="btn btn-primary">Back</button></a><br>
         <hr><br>
-        <video width="400" class="view-video">
+        <video  class="view-video" controls>
             <source src="{{ URL::asset("$video->video") }}" type="video/mp4">
             Your browser does not support HTML video.
         </video>
