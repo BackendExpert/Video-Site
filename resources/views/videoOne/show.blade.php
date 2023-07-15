376591f03ce7223c7eb1d215b33dd2c2ee02e8d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-15 18:09:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/videoOne/show.blade.php
+++ resources/views/videoOne/show.blade.php
@@ -10,8 +10,11 @@
     <div class="one-video">
         <a href="{{ url('') }}"><button class="btn btn-primary">Back</button></a><br>
         <hr><br>
+        <video width="400" class="view-video">
+            <source src="{{ URL::asset("$video->video") }}" type="video/mp4">
+            Your browser does not support HTML video.
+        </video>
 
-        
     </div>
 </div>
 
