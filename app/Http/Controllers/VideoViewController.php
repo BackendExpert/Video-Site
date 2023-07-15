06b857e03cdb03a532a6396c8c032420cac2ba14
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-15 17:59:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VideoViewController.php
+++ app/Http/Controllers/VideoViewController.php
@@ -14,27 +14,9 @@ class VideoViewController extends Controller
         return view('/videoOne.show')->with('video', $video);    
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
