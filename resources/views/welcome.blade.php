d6318b015f37c724c6ff4df582672790a81101e1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-15 17:55:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,7 +109,7 @@
                     <div class="home-body">
                         @auth
                             @foreach ($video as $vid)
-                                <a href="{{ url('/videoOne/show/' . $vid->id) }}" class="one-video-link">
+                                <a href="{{ url('/videoOne/' . $vid->id) }}" class="one-video-link">
                                     <div class="one-video">
                                         <div class="row">
                                             <div class="col-lg-4">
