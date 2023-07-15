20d0ca77dd281e3a94f5a830c216a61b46442381
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-15 18:07:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/videoOne/show.blade.php
+++ resources/views/videoOne/show.blade.php
@@ -8,7 +8,8 @@
 
 <div class="container">
     <div class="one-video">
-        <a href="{{ url('') }}"><button class="btn btn-primary">Back</button></a>
+        <a href="{{ url('') }}"><button class="btn btn-primary">Back</button></a><br>
+        <hr><br>
     </div>
 </div>
 
