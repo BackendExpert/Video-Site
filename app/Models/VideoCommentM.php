0971bcf9c6455a034a3c6e916da84237d817acbc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 07:59:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Models/VideoCommentM.php
+++ app/Models/VideoCommentM.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class VideoCommentM extends Model
 {
     use HasFactory;
+    protected $table = 'videos__comments';
+    protected $primaryKey = 'id';
+    protected $fillable = [       
+        'video_id',
+        'commenter',
+        'description',
+        'created_at',
+        'updated_at',
+    ];
 }
