e2f64e84aa48323ac60eaee8383b88ecc9220436
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 09:33:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VideoCommentController.php
+++ app/Http/Controllers/VideoCommentController.php
@@ -19,10 +19,10 @@ class VideoCommentController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    // public function create()
-    // {
-    //     return view('/videoOne/comment');
-    // }
+    public function create()
+    {
+        return view('/videoOne/comment');
+    }
 
     /**
      * Store a newly created resource in storage.
