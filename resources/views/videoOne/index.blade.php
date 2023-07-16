b65086f7445cf338edb758bb7d5ee7a172740efd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 08:49:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/videoOne/index.blade.php
+++ resources/views/videoOne/index.blade.php
@@ -20,22 +20,22 @@
         <a href="{{ url('') }}"><button class="btn btn-primary">Back</button></a><br>
         <hr><br>
         <video  class="view-video" controls>
-            <source src="{{ URL::asset("$video->video") }}" type="video/mp4">
+            <source src="{{ URL::asset("$video_one->video") }}" type="video/mp4">
             Your browser does not support HTML video.
         </video>
         <hr>
         <h3>{{ $video->video_title }}</h3>
         <div class="row">
             <div class="col-lg-6">
-                <h5>Uploader : {{ $video->uploader }}</h5>
+                <h5>Uploader : {{ $video_one->uploader }}</h5>
             </div>
             <div class="col-lg-6 add-time">
-                <h5>{{ $video->created_at }}</h5>
+                <h5>{{ $video_one->created_at }}</h5>
             </div>
         </div>
         <hr>
         <h4>Description</h4>
-        <p>{{ $video->description }}</p>
+        <p>{{ $video_one->description }}</p>
 
         <hr>
         <h4>Comments</h4>
