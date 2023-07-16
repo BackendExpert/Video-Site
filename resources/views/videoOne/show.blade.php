a022a4a62d192c4ab5afd6740bced2d80101393f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 09:44:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/videoOne/show.blade.php
+++ resources/views/videoOne/show.blade.php
@@ -41,7 +41,7 @@
         <h4>Comments</h4>
         <br>
 
-        <a href="/videoComment/create">sad</a>
+        <a href="/videoComment/create"><button class="btn btn-success">Add New Comment</button></a>
 
         <br><hr>
 
