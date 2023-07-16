45aed31cbbb994830de389d7e8501727551280b9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 08:38:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VideoViewController.php
+++ app/Http/Controllers/VideoViewController.php
@@ -11,7 +11,7 @@ class VideoViewController extends Controller
     public function show(string $id)
     {
         $video = VideoM::find($id);
-        return view('/videoOne.show')->with('video', $video);    
+        return view('/videoOne.index')->with('video', $video);    
     }
 
     public function update(Request $request, string $id)
