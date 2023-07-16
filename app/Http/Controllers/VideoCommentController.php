65610f7df9abb1c644ff80b7ecde7f77bb79f4ad
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 08:35:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/Http/Controllers/VideoCommentController.php
+++ app/Http/Controllers/VideoCommentController.php
@@ -21,7 +21,7 @@ class VideoCommentController extends Controller
      */
     public function create()
     {
-        return view('videoOne/show');
+        return view('videoOne');
     }
 
     /**
