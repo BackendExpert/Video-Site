13e69f041d2910d0700fb73eff6ee977842c8cf5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-07-16 08:22:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- resources/views/videoOne/show.blade.php
+++ resources/views/videoOne/show.blade.php
@@ -53,7 +53,7 @@
             </ul>
         @endif
 
-        <form action="{{ url('/videoOne.show') }}" method="post">
+        <form action="{{ url('/') }}" method="post">
             @csrf
         <h5>New Comment</h5>
             <input type="hidden" name="video_id" value="{{ $video->id }}">
